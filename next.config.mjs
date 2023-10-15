935c46c457dbe259810337ec669248dd5a31bb6a
Possawat Suksai <markkung08@gmail.com>
2023-10-15 20:38:34 +0800

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@ import { withContentlayer } from 'next-contentlayer'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  reactStrictMode: true
 }
 
 export default withContentlayer(nextConfig)
